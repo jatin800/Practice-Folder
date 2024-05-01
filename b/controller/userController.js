772abbe0d3a9890a83b47b19da3480eeb6dd46d5
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-05-01 22:53:26 +0530

    readUser functionality done

--- b/controller/userController.js
+++ b/controller/userController.js
@@ -10,3 +10,13 @@ export const createUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const readUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findOne({ _id: userId });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- b/route/userRoute.js
+++ b/route/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controller/userController.js";
+import { createUser, readUser } from "../controller/userController.js";
 
 const router = express.Router();
 
 router.post("/create", createUser);
+router.get("/read/:id",readUser);
 
 export default router;
