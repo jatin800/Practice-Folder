7255d7f068c49b2d3730fed6e9dbde91bdec8509
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-05-02 02:37:54 +0530

    impelement cors on backend and post request by axios and finally data will reflect on database

--- b/server.js
+++ b/server.js
@@ -1,13 +1,15 @@
 import express from "express";
+import cors from "cors";
 import { connectToMongoDB } from "./db/DB.js";
 import router from "./route/userRoute.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 connectToMongoDB();
 
-app.use(router)
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
--- f/src/components/Form.jsx
+++ f/src/components/Form.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
+import axios from "axios";
 
 export default function Form() {
+  const [formInput, setFormInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    setFormInput({
+      ...formInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/create",
+        formInput
+      );
+      setFormInput({ name: "", email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className='container'>
@@ -10,22 +38,42 @@ export default function Form() {
           <div>
             <label>
               <span className='span'>Name:</span>
-              <input className='input' type='text' />
+              <input
+                className='input'
+                type='text'
+                name='name'
+                value={formInput.name}
+                onChange={handleInput}
+              />
             </label>
           </div>
           <div>
             <label>
               <span className='span'>email:</span>
-              <input className='input' type='email' />
+              <input
+                className='input'
+                type='email'
+                name='email'
+                value={formInput.email}
+                onChange={handleInput}
+              />
             </label>
           </div>
           <div>
             <label>
               <span className='span'>password:</span>
-              <input className='input' type='password' />
+              <input
+                className='input'
+                type='password'
+                name='password'
+                value={formInput.password}
+                onChange={handleInput}
+              />
             </label>
           </div>
-          <button type='submit'>Submit</button>
+          <button type='submit' onClick={handleSubmit}>
+            Submit
+          </button>
         </form>
       </div>
       <hr />
