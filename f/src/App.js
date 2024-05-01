03e190c13c37ef3e730e9c4c616a544e997f684c
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-05-01 19:03:40 +0530

    form completed with scss styles

--- f/src/App.js
+++ f/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className='container'>
+      <h1>Create User</h1>
+      <form>
+        <div>
+          <label>
+            <span className='span'>Name:</span>
+            <input className='input' type='text' />
+          </label>
+        </div>
+        <div>
+          <label>
+            <span className='span'>email:</span>
+            <input className='input' type='email' />
+          </label>
+        </div>
+        <div>
+          <label>
+            <span className='span'>password:</span>
+            <input className='input' type='password' />
+          </label>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
